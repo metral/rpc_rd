b3945b8d717c5550f77c28d1c9f904108d36723c
Mike Metral <metral@gmail.com>
2014-05-05 09:22:31 -0700

    Remove faketime

--- qcow_image/convert_to_vhd.sh
+++ qcow_image/convert_to_vhd.sh
@@ -9,7 +9,7 @@ cd xen*/tools/blktap2/vhd
 export LD_LIBRARY_PATH=lib/
 qemu-img convert -O raw $INPUT_PATH /tmp/$QEMU_FILENAME.raw
 
-faketime '2014-01-01' ./vhd-util convert -s 0 -t 1 -i /tmp/$QEMU_FILENAME.raw -o /tmp/0_$QEMU_FILENAME.vhd
-faketime '2014-01-01' ./vhd-util convert -s 1 -t 2 -i /tmp/0_$QEMU_FILENAME.vhd -o $OUTPUT_PATH/$QEMU_FILENAME.vhd
+./vhd-util convert -s 0 -t 1 -i /tmp/$QEMU_FILENAME.raw -o /tmp/0_$QEMU_FILENAME.vhd
+./vhd-util convert -s 1 -t 2 -i /tmp/0_$QEMU_FILENAME.vhd -o $OUTPUT_PATH/$QEMU_FILENAME.vhd
 
 rm -rf /tmp/*$QEMU_FILENAME*
